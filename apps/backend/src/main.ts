64796d778a26442ab8b5b524869c60ecf57ffcc9
Filipe Esposito <filipe.tessariesposito@kloeckner.com>
2025-11-19 20:54:41 +0100

    Updates CORS on the backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,10 +9,18 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
   const port = process.env.PORT || 3000;
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
