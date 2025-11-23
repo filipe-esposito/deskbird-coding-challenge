81cb3260da021cdab9f77ffba455595a774660c6
Filipe Esposito <filipe.tessariesposito@kloeckner.com>
2025-11-23 16:47:30 +0100

    Improves readability on unit tests for `UserListComponent`

--- apps/frontend/src/app/features/users/user-list.spec.ts
+++ apps/frontend/src/app/features/users/user-list.spec.ts
@@ -8,21 +8,23 @@ import {
   queryElementByTestId,
   queryElementsByTestId,
 } from '../../shared/utils/unit-tests';
+import { IUser } from '@deskbird-coding-challenge/shared-lib';
 
 const mockUsers = [
   { id: 1, name: 'Alice', username: 'alice@example.com', isAdmin: false },
   { id: 2, name: 'Bob', username: 'bob@example.com', isAdmin: true },
 ];
 
+const usersServiceSpy = {
+  getUsers: jest.fn(),
+};
+const authServiceSpy = {
+  getCurrentUser: jest.fn(),
+};
+
 describe('UserListComponent', () => {
   let fixture: ComponentFixture<UserListComponent>;
   let component: UserListComponent;
-  const usersServiceSpy = {
-    getUsers: jest.fn(),
-  };
-  const authServiceSpy = {
-    getCurrentUser: jest.fn(),
-  };
 
   beforeEach(async () => {
     usersServiceSpy.getUsers.mockClear();
@@ -40,15 +42,11 @@ describe('UserListComponent', () => {
 
   describe('if the current user is an admin', () => {
     beforeEach(() => {
-      authServiceSpy.getCurrentUser.mockReturnValue(() => ({
+      fixture = configureFixtureWithCurrentUser({
         id: 42,
         isAdmin: true,
-      }));
-      usersServiceSpy.getUsers.mockReturnValue(() => mockUsers);
-
-      fixture = TestBed.createComponent(UserListComponent);
+      });
       component = fixture.componentInstance;
-      fixture.detectChanges();
     });
 
     it('the `Create user` button should be visible', () => {
@@ -78,15 +76,11 @@ describe('UserListComponent', () => {
 
   describe('if the current user is NOT an admin', () => {
     beforeEach(() => {
-      authServiceSpy.getCurrentUser.mockReturnValue(() => ({
+      fixture = configureFixtureWithCurrentUser({
         id: 42,
         isAdmin: false,
-      }));
-      usersServiceSpy.getUsers.mockReturnValue(() => mockUsers);
-
-      fixture = TestBed.createComponent(UserListComponent);
+      });
       component = fixture.componentInstance;
-      fixture.detectChanges();
     });
 
     it('the `Create user` button should NOT be visible', () => {
@@ -115,15 +109,11 @@ describe('UserListComponent', () => {
   });
 
   it('the `Delete user` button should not be visible on the user list for the list item related to the current user', () => {
-    const currentUser = {
+    fixture = configureFixtureWithCurrentUser({
       id: 2,
       isAdmin: true,
-    };
-    authServiceSpy.getCurrentUser.mockReturnValue(() => currentUser);
-    usersServiceSpy.getUsers.mockReturnValue(() => mockUsers);
-    fixture = TestBed.createComponent(UserListComponent);
+    });
     component = fixture.componentInstance;
-    fixture.detectChanges();
 
     const deleteUserButtons = queryElementsByTestId(fixture, 'delete-user-btn');
 
@@ -132,15 +122,11 @@ describe('UserListComponent', () => {
 
   describe('should display the correct role label for users', () => {
     it('when a user from the list is an admin', () => {
-      const currentUser = {
+      fixture = configureFixtureWithCurrentUser({
         id: 1,
         isAdmin: true,
-      };
-      authServiceSpy.getCurrentUser.mockReturnValue(() => currentUser);
-      usersServiceSpy.getUsers.mockReturnValue(() => mockUsers);
-      fixture = TestBed.createComponent(UserListComponent);
+      });
       component = fixture.componentInstance;
-      fixture.detectChanges();
 
       const result = component.displayUserRole(true);
 
@@ -148,15 +134,11 @@ describe('UserListComponent', () => {
     });
 
     it('when a user from the list is NOT an admin', () => {
-      const currentUser = {
+      fixture = configureFixtureWithCurrentUser({
         id: 1,
         isAdmin: false,
-      };
-      authServiceSpy.getCurrentUser.mockReturnValue(() => currentUser);
-      usersServiceSpy.getUsers.mockReturnValue(() => mockUsers);
-      fixture = TestBed.createComponent(UserListComponent);
+      });
       component = fixture.componentInstance;
-      fixture.detectChanges();
 
       const result = component.displayUserRole(false);
 
@@ -164,3 +146,14 @@ describe('UserListComponent', () => {
     });
   });
 });
+
+function configureFixtureWithCurrentUser(
+  currentUser: IUser
+): ComponentFixture<UserListComponent> {
+  authServiceSpy.getCurrentUser.mockReturnValue(() => currentUser);
+  usersServiceSpy.getUsers.mockReturnValue(() => mockUsers);
+  const fixture = TestBed.createComponent(UserListComponent);
+  fixture.detectChanges();
+
+  return fixture;
+}
